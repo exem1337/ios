4a39166716cb582171635872b1c8f7d298dfd3c6
alexander.chernyakov <alexander.chernyakov@waveaccess.global>
2022-10-28 12:09:44 +0500

    навбар

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -2,20 +2,58 @@
   <q-layout view="lHh Lpr lFf">
     <q-header elevated bordered>
       <q-toolbar class="bg-brown-8">
-        <q-btn
-          flat
-          dense
-          round
-          icon="menu"
-          aria-label="Menu"
-          @click="toggleLeftDrawer"
-        />
-
         <q-toolbar-title> АОС </q-toolbar-title>
+        <q-btn-dropdown stretch flat label="Экспертная система">
+          <q-list>
+            <q-item-label header>Экспертная система</q-item-label>
+            <q-item
+              v-for="n in 3"
+              :key="`x.${n}`"
+              clickable
+              v-close-popup
+              tabindex="0"
+            >
+              <q-item-section avatar>
+                <q-avatar icon="folder" color="secondary" text-color="white" />
+              </q-item-section>
+              <q-item-section>
+                <q-item-label>Photos</q-item-label>
+                <q-item-label caption>February 22, 2016</q-item-label>
+              </q-item-section>
+              <q-item-section side>
+                <q-icon name="info" />
+              </q-item-section>
+            </q-item>
+            <q-separator inset spaced />
+            <q-item-label header>Files</q-item-label>
+            <q-item
+              v-for="n in 3"
+              :key="`y.${n}`"
+              clickable
+              v-close-popup
+              tabindex="0"
+            >
+              <q-item-section avatar>
+                <q-avatar
+                  icon="assignment"
+                  color="primary"
+                  text-color="white"
+                />
+              </q-item-section>
+              <q-item-section>
+                <q-item-label>Vacation</q-item-label>
+                <q-item-label caption>February 22, 2016</q-item-label>
+              </q-item-section>
+              <q-item-section side>
+                <q-icon name="info" />
+              </q-item-section>
+            </q-item>
+          </q-list>
+        </q-btn-dropdown>
+        <q-btn to="/courses" stretch flat label="Дисциплины" />
+        <q-btn stretch flat label="Выход" />
       </q-toolbar>
     </q-header>
-
-    <q-drawer v-model="leftDrawerOpen" persistent bordered />
 
     <q-page-container>
       <router-view />
@@ -23,12 +61,4 @@
   </q-layout>
 </template>
 
-<script setup lang="ts">
-import { ref } from 'vue';
-const leftDrawerOpen = ref(true);
-leftDrawerOpen.value = false;
-
-function toggleLeftDrawer() {
-  leftDrawerOpen.value = !leftDrawerOpen.value;
-}
-</script>
+<script setup lang="ts"></script>
