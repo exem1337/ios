4b57cd4fb5c71937846244843975dc648ee234c5
alexander.chernyakov <alexander.chernyakov@waveaccess.global>
2023-04-21 19:02:16 +0500

    asdasd

--- src/constants/difficultyMap.const.ts
+++ src/constants/difficultyMap.const.ts
@@ -2,8 +2,8 @@ import { ERuleResultLevels } from "src/enums/rule.enum";
 
 export const difficultyMap = new Map<ERuleResultLevels, number>([
   [ERuleResultLevels.Newbie, 5],
-  [ERuleResultLevels.Intern, 40],
-  [ERuleResultLevels.Master, 60],
-  [ERuleResultLevels.Professional, 80],
+  [ERuleResultLevels.Intern, 50],
+  [ERuleResultLevels.Master, 65],
+  [ERuleResultLevels.Professional, 85],
   [ERuleResultLevels.Expert, 100],
 ])
